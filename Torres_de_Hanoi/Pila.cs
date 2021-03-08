245afb7038fac9563db2789c165b4f995f3a8f53
sergisirvent <sergisise@gmail.com>
2021-03-08 19:31:19 +0100

    Commit Implementacion Clase Pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -9,34 +9,89 @@ namespace Torres_de_Hanoi
     class Pila
     {
         public int Size { get; set; }
+
         /* TODO: Elegir tipo de Top
         public int Top { get; set; }
         public String Top { get; set; }        
         */
+
+        public int Top { get; set; }
+
         /* TODO: Elegir tipo de Elementos
         public Disco[] Elementos { get; set; }
         public List<Disco> Elementos { get; set; }
         */
 
+        public List<Disco> Elementos { get; set; }
+
         /* TODO: Implementar métodos */
         public Pila()
         {
-
+            Size = 0;
+            Top = 0;
+            Elementos = new List<Disco>();
+            
         }
 
         public void push(Disco d)
         {
+            if(Size == 0 )
+            {
+
+                Elementos.Add(d);
+                Size++;
+                Top = d.Valor;
+
+            }else
+            {
+                if (d.Valor < Top)
+                {
+                    Elementos.Add(d);
+                    Size++;
+                    Top = d.Valor;
+                }
+                else
+                {
+                    Console.WriteLine("No es posible añadir el disco con valor " + d.Valor);
+                }
+                
+            }
+            
+            
 
         }
 
         public Disco pop()
         {
-            return null;
+
+            Disco extraido =  Elementos[Size-1];
+            Elementos.RemoveAt(Size - 1);
+            
+            Size--;
+            if(Size == 0)
+            {
+                Top = 0;
+            }
+            else
+            {
+                Top = Elementos[Size - 1].Valor;
+            }
+
+            return extraido;
+
         }                
 
         public bool isEmpty()
         {
-            return true;
+            if(Elementos.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
         }
 
     }
--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -10,9 +10,35 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
+            Pila pila = new Pila();
 
+            Disco disco1 = new Disco();
+            disco1.Valor = 5;
+
+            Disco disco2 = new Disco();
+            disco2.Valor = 10;
+
+            Disco disco3 = new Disco();
+            disco3.Valor = 3;
+
+            Disco disco4 = new Disco();
+            disco4.Valor = 2;
+
+            pila.push(disco1);
+            pila.push(disco2);
+            pila.push(disco3);
+            //pila.push(disco4);
+
+           // Console.WriteLine("Elementos finales " + pila.isEmpty() + Environment.NewLine);
+            foreach(Disco d in pila.Elementos)
+            {
+                Console.WriteLine(d.Valor);
+            }
+            Disco popDisco = pila.pop();
+            Console.WriteLine("Valor del disco extraido " +popDisco.Valor);
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
+
             Console.ReadKey();
         }
     }
